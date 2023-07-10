3e983bed9e14c1487b21bd073973404d4be1fa4b
AbenezerKb <aben.jacob3@gmail.com>
2023-07-10 15:10:32 +0300

    patch embedding and attention

---
+++ vision-transformer.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+
+
+class PathEmbed(nn.Module):
+    def __init__(self, img_size, patch_size, in_chans=3, embed_dim=768):
+        super().__init__()
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.n_patches = (img_size // patch_size) ** 2
+        self.proj = nn.Conv2d(
+            in_chans,
+            embed_dim,
+            kernel_size=patch_size,
+            stride=patch_size,
+            )
+
+    def forward(self, x):
+        x = self.proj(
+                x
+            )
+        x = x.flatten(2)
+        x = x.transpose(1, 2)
+
+
+class Attention(nn.Module):
+
+    def __init__(self, dim, n_heads=12, qkv_bias=True, attn_p=0., proj_p=0.):
+        super().__init__()
+        self.n_heads = n_heads
+        self.dim = dim
+        self.n_head_dim = dim // n_heads
+        self.scale = self.n_head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_p)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_p)
+
+    def forward(self, x):
+        n_samples, n_tokens, dim = x.shape
+        if dim != self.dim:
+            raise ValueError
+        qkv = self.qkv(x)
+        qkv = qkv.reshape(
+            n_samples, n_tokens, 3, self.n_heads, self.head_dim
+        )
+        qkv = qkv.permute(
+            2, 0, 3, 1, 4
+        )
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        k_t = k.transpose(-2, -1)
+        dp = (
+            q @ k_t
+        ) * self.scale
+        attn = dp.softmax(dim-1)
+
+        weighted_avg = attn @ v
+        weighted_avg = weighted_avg.transpose(
+            1, 2
+        )
+        weighted_avg = weighted_avg.flatten(2)
+        x = self.proj(weighted_avg)
+        x = self.proj_drop(x)
+
+        return x
+
